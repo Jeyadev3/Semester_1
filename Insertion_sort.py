738f43ac6d0bacfaf5cb09bb96d90199cc01e491
Jeyadev <jeyadev03@gmail.com>
2026-02-16 21:30:16 +0530

    Add files via upload

---
+++ Insertion_sort.py
@@ -0,0 +1,14 @@
+def Insertionsort(arr):
+    for i in range(1,len(arr)):
+        key=arr[i]
+        j=i-1
+        while j>=0 and arr[j]>key:
+            arr[j+1]=arr[j]
+            j-=1
+        arr[j+1]=key
+    return arr
+
+
+
+arr=[3,5,2,1,4,6]
+print(Insertionsort(arr))
